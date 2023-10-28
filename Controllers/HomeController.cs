c8a99d871d4d32d72547157e4b4b34f64c26a6e6
Ramanatha Kamath <ramanatha.kamath94@gmail.com>
2023-10-28 12:03:17 +0530

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using lifedashboard.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace lifedashboard.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Attendance()
+        {
+            return View();
+        }
+
+        public IActionResult FeeCollection()
+        {
+            return View();
+        }
+
+        public IActionResult MemberDetails()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Controllers/MembershipController.cs
@@ -0,0 +1,20 @@
+﻿using lifedashboard.Data;
+using lifedashboard.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lifedashboard.Controllers
+{
+    public class MembershipController : Controller
+    {
+        DBUtil dB=new DBUtil();
+        Members members = new Members();
+
+        public IActionResult Membership()
+        {
+            return View();
+        }
+
+        public void 
+
+    }
+}
